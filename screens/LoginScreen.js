4dfee97d7ed571974b469a17546741f5b1b4a293
Ngcc3006 <tranbaongoc.hcm2004@gmail.com>
2025-09-21 17:04:41 +0700

    Thêm Pop Up khi đăng nhập đúng/sai

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert, ActivityIndicator } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert, ActivityIndicator, Platform } from 'react-native';
 import { AuthContext } from '../context/AuthContext';
+
 
 export default function LoginScreen({ navigation }) {
   const [username, setUsername] = useState('');
@@ -9,17 +10,29 @@ export default function LoginScreen({ navigation }) {
 
   const handleLogin = async () => {
     if (!username || !password) {
-      Alert.alert('Thông báo', 'Vui lòng nhập đầy đủ thông tin');
-      return;
+    if (Platform.OS === "web") {
+      alert("Vui lòng nhập đầy đủ thông tin");
+    } else {
+      Alert.alert("Thông báo", "Vui lòng nhập đầy đủ thông tin");
     }
+    return;
+  }
 
-    try {
-      await login(username, password);
-      Alert.alert('Thành công', 'Đăng nhập thành công!');
-      navigation.navigate('Profile');
-    } catch (error) {
-      Alert.alert('Lỗi', error.message);
+  try {
+    await login(username, password);
+    if (Platform.OS === "web") {
+      alert("Đăng nhập thành công!");
+    } else {
+      Alert.alert("Thành công", "Đăng nhập thành công!");
     }
+    navigation.navigate('Profile');
+  } catch (error) {
+    if (Platform.OS === "web") {
+      alert(error.message);
+    } else {
+      Alert.alert("Lỗi", error.message);
+    }
+  }
   };
 
   return (
